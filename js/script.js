742a208449c7c89b1ca9c344e3860ad11e0b85f6
Shivanshu sood <Shvnshsood@gmail.com>
2022-01-26 08:13:05 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.slider_part').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
